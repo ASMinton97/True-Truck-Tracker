248288adc5a371aa44d8a03f8e099fae09b101e2
ASMinton97 <a.minton97@gmail.com>
2020-01-16 10:25:39 -0700

    Got marker on users location. Working on Fetch

--- TrueTruckTracker/App.js
+++ TrueTruckTracker/App.js
@@ -19,98 +19,90 @@ export default class App extends React.Component {
     }
   };
 
-  componentWillMount() {
-    this.fetchData();
-  }
+  // componentWillMount() {
+  //   this.fetchData();
+  // }
 
-  fetchData = () => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const latitude = Number(position.coords.latitude.toFixed(6));
-        const longitude = Number(position.coords.longitude.toFixed(6));
-        const { pageToken } = this.state;
-        const urlOne = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=500&type=restaurant&key=AIzaSyBiBHHoqdnd4k6tX4W_qmGl9n1_6N8Z4Os`;
-        const urlSecond = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=500&type=restaurant&key=Your_Api_Key&pagetoken=${pageToken}`;
+  // fetchData = () => {
+  //   navigator.geolocation.getCurrentPosition(
+  //     (position) => {
+  //       const latitude = Number(position.coords.latitude.toFixed(6));
+  //       const longitude = Number(position.coords.longitude.toFixed(6));
+  //       const { pageToken } = this.state;
+  //       const urlOne = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=500&type=restaurant&key=AIzaSyBiBHHoqdnd4k6tX4W_qmGl9n1_6N8Z4Os`;
+  //       const urlSecond = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=500&type=restaurant&key=Your_Api_Key&pagetoken=${pageToken}`;
 
-        let url = pageToken === '' ? urlOne : urlSecond;
-        console.log(url);
-        console.log("url");
-        this.setState({ loading: true });
-        fetch(url)
-          .then(res => {
-            return res.json()
-          })
-          .then(res => {
+  //       let url = pageToken === '' ? urlOne : urlSecond;
+  //       console.log(url);
+  //       console.log("url");
+  //       this.setState({ loading: true });
+  //       fetch(url)
+  //         .then(res => {
+  //           return res.json()
+  //         })
+  //         .then(res => {
 
-            const arrayData = _.uniqBy([...this.state.data, ...res.results], 'id');
+  //           const arrayData = _.uniqBy([...this.state.data, ...res.results], 'id');
 
-            this.setState({
-              siteTitle: "Restaurants Nearby",
-              data: pageToken === '' ? res.results : arrayData,
-              loading: false,
-              refreshing: false,
-              pageToken: res.next_page_token,
-              positionLatitude: latitude,
-              positionLongitude: longitude
-            });
+  //           this.setState({
+  //             siteTitle: "Restaurants Nearby",
+  //             data: pageToken === '' ? res.results : arrayData,
+  //             loading: false,
+  //             refreshing: false,
+  //             pageToken: res.next_page_token,
+  //             positionLatitude: latitude,
+  //             positionLongitude: longitude
+  //           });
 
-          })
-          .catch(error => {
-            console.log(error);
-            this.setState({ loading: false });
-          });
-      }
-    );
-  };
+  //         })
+  //         .catch(error => {
+  //           console.log(error);
+  //           this.setState({ loading: false });
+  //         });
+  //     }
+  //   );
+  // };
 
-  renderSeparator = () => {
-    return (
-      <View style={{ height: 1, width: "86%", backgroundColor: '#CED0CE', marginLeft: "14%" }} />
-    );
-  };
+  // renderSeparator = () => {
+  //   return (
+  //     <View style={{ height: 1, width: "86%", backgroundColor: '#CED0CE', marginLeft: "14%" }} />
+  //   );
+  // };
 
-  renderHeader = () => {
-    return (
-      <Text style={{ alignSelf: "center", fontWeight: "bold", fontSize: 20, marginBottom: 10 }}>{this.state.siteTitle}</Text>
-    )
-  };
+  // renderHeader = () => {
+  //   return (
+  //     <Text style={{ alignSelf: "center", fontWeight: "bold", fontSize: 20, marginBottom: 10 }}>{this.state.siteTitle}</Text>
+  //   )
+  // };
 
-  renderFooter = () => {
-    if (this.state.pageToken === undefined) return null;
+  // renderFooter = () => {
+  //   if (this.state.pageToken === undefined) return null;
 
-    return (
-      <View style={{ paddingVertical: 20, borderTopWidth: '#CDE0CE' }}>
-        <ActivityIndicator animating size='large' />
-      </View>
-    )
-  };
+  //   return (
+  //     <View style={{ paddingVertical: 20, borderTopWidth: '#CDE0CE' }}>
+  //       <ActivityIndicator animating size='large' />
+  //     </View>
+  //   )
+  // };
 
-  handleRefresh = () => {
-    this.state({
-      pageToken: '',
-      refreshing: true
-    },
-      () => {
-        this.fetchData();
-      }
-    );
-  };
+  // handleRefresh = () => {
+  //   this.state({
+  //     pageToken: '',
+  //     refreshing: true
+  //   },
+  //     () => {
+  //       this.fetchData();
+  //     }
+  //   );
+  // };
 
-  handleLoadMore = () => {
-    this.fetchData();
-  };
+  // handleLoadMore = () => {
+  //   this.fetchData();
+  // };
 
   render() {
-    console.log("Latitude: " + this.state.positionLatitude + " Longitude: " + this.state.positionLongitude)
     return (
-      <MapView style={{ flex: 1 }}
-        initialRegion={{
-            latitude: this.state.positionLatitude,
-            longitude: this.state.positionLongitude,
-            latitudeDelta: 0.1292,
-            longitudeDelta: 0.1292
-        }}
-      />
+      <Places/>
     );
   }
 }
--- TrueTruckTracker/src/components/places.js
+++ TrueTruckTracker/src/components/places.js
@@ -1,36 +1,80 @@
-import React, {Component} from 'react';
-import {View, StyleSheet, FlatList, Alert} from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, FlatList, Alert } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
-export default class places extends Component{
+export default class places extends Component {
     state = {
         apiKey: '2zReQTlfVvoUM_NeXqba2jiYfbYN3e4LksUiFuivMlv_CsJc8hlMYvrVbZuM0ruv8sjpj-R6aljRzyyWBNVKvMyEEgPQI1oCadbsGV-kmpxACjunydwkOdVZmSwaXnYx',
-
+        userLatitude: 0,
+        userLongitude: 0,
+        markers: []
     };
+
+    componentWillMount() {
+        this.findCoordinates();
+    }
+
+    componentDidMount(){
+        this.getData();
+    }
 
     findCoordinates = () => {
         navigator.geolocation.getCurrentPosition(
             (position) => {
                 const latitude = Number(position.coords.latitude.toFixed(6));
                 const longitude = Number(position.coords.longitude.toFixed(6));
-                this.setState({ location });
-                const url = `https://api.yelp.com/v3/foodtrucks/search?latitude=${latitude}&${longitude}&radius_filter=500`;
-            
-            fetch(url, {
-                method: 'GET',
-                headers: {Authorization: 'Bearer ' & this.state.apiKey}
-            })
-            .then(response => response.json())
-            .then(json =>{
-                console.log()
-            }),
-            error => Alert.alert(error.message),
-            {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+                this.setState({ userLatitude: latitude, userLongitude: longitude });
+                console.log("Latitude: " + this.state.userLatitude + " Longitude: " + this.state.userLongitude + " When is this happening");
+                this.forceUpdate();
             });
     };
-    render(){
-        return(
-            <View>
-            </View>
+
+    getData(){
+        let url = `https://api.yelp.com/v3/foodtrucks/search?latitude=${this.state.userLatitude}&longitude=${this.state.userLongitude}&radius_filter=500`;
+        fetch(url, {
+            method: 'GET',
+            headers: { Authorization: 'Bearer ', apiKey: this.state.apiKey}
+        })
+            .then(response => response.json())
+            .then(json => {
+                for(let i=0; i < json.body.businesses.length; i++){
+                    console.log(i);
+                    this.state.markers.push({
+                        name: json.body.businesses[i].name,
+                        coordinates: json.body.businesses[i].coordinates,
+                        rating: json.body.businesses[i].rating,
+                        price: json.body.businesses[i].price,
+                        id: json.body.businesses[i].id,
+                        image_url: json.body.business[i].image_url,
+                        phone: json.body.businesses[i].phone
+                    })
+                }
+                //this.setState({url: json.body.businesses})
+                console.log("IS THIS HAPPENING AT ALL?")
+                console.log(markers);
+            }),
+            error => Alert.alert(error.message),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    }
+
+    render() {
+        console.log("Latitude: " + this.state.userLatitude + " Longitude: " + this.state.userLongitude)
+        return (
+            <MapView
+                style={{ flex: 1 }}
+                region={{
+                    latitude: this.state.userLatitude,
+                    longitude: this.state.userLongitude,
+                    latitudeDelta: 0.1292,
+                    longitudeDelta: 0.1292
+                }}
+            >
+                <Marker
+                    coordinate={{ latitude: this.state.userLatitude, longitude: this.state.userLongitude}}
+                    title='Here I am!'
+                    description="Here is how description works"
+                />
+            </MapView>
         )
     }
 }
