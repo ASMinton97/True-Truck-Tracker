880a4ec6c464014e6c07824197c06e4be3b32e53
ASMinton97 <a.minton97@gmail.com>
2020-02-17 19:47:48 -0700

    Added login and register page

---
+++ TrueTruckTracker/src/components/register.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, TouchableOpacity, Alert, Text, Image, AsyncStorage, Linking } from 'react-native';
+import SettingsList from 'react-native-settings-list';
+import { NavigationContainer, DefaultTheme, } from '@react-navigation/native';
